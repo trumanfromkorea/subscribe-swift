55b36210736ed571a7a1ea1e79f080b0888b0a5b
Jaehoon Jang <jangjh971008@naver.com>
2022-01-11 20:07:15 +0900

    initial commit

---
+++ subscribe/subscribeApp.swift
@@ -0,0 +1,17 @@
+//
+//  subscribeApp.swift
+//  subscribe
+//
+//  Created by 장재훈 on 2022/01/11.
+//
+
+import SwiftUI
+
+@main
+struct subscribeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}

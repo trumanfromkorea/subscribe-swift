82f9353b44f0bc5cf521d16dd1e59157f2ee2bf6
Jaehoon Jang <jangjh971008@naver.com>
2022-01-11 20:33:18 +0900

    add Firebase

--- subscribe/ContentView.swift
+++ subscribe/ContentView.swift
@@ -5,12 +5,22 @@
 //  Created by 장재훈 on 2022/01/11.
 //
 
+import AuthenticationServices
 import SwiftUI
 
 struct ContentView: View {
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        SignInWithAppleButton(.signIn) { request in
+            request.requestedScopes = [.fullName, .email]
+        } onCompletion: { result in
+//            switch result {
+//            case let .success(authResults):
+//                print("Authorization successful")
+//            case let .error(error):
+//                print("FAILED : \(error.localizedDescription)")
+//            }
+            print(result)
+        }.signInWithAppleButtonStyle(.black)
     }
 }
 
